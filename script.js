93b550efc0af22693ee1524c1f33fe77964900fb
sofradks <stud0000261491@study.utmn.ru>
2024-05-10 22:54:31 +0500

    test

--- script.js
+++ script.js
@@ -1,155 +1,163 @@
-if (document.readyState === 'loading') 
-{
-    document.addEventListener('DOMContentLoaded', init)
-} else 
-{
-    init()
+
+class Topping{
+    constructor(name, smallPrice, smallCalories, bigPrice, bigCalories){
+        this.name = name;
+        this.smallPrice = smallPrice;
+        this.smallCalories = smallCalories;
+        this.bigPrice = bigPrice;
+        this.bigCalories = bigCalories;
+    }
 }
 
-function init() 
-{
-    const data = 
-    {
-        name: 'Каталог товаров',
-        hasChildren: true,
-        items: 
-        [
-            {
-                name: 'Мойки',
-                hasChildren: true,
-                items: [
-                    {
-                        name: 'Ulgran',
-                        hasChildren: true,
-                        items: 
-                        [
-                            {
-                                name: 'Smth',
-                                hasChildren: false,
-                                items: []
-                            },
-                            {
-                                name: 'Smth',
-                                hasChildren: false,
-                                items: []
-                            }
-                        ]
-                    },
-                    {
-                        name: 'Vigro Mramor',
-                        hasChildren: false,
-                        items: []
-                    },
-                    {
-                        name: 'Handmade',
-                        hasChildren: true,
-                        items: 
-                        [
-                            {
-                                name: 'Smth',
-                                hasChildren: false,
-                                items: []
-                            },
-                            {
-                                name: 'Smth',
-                                hasChildren: false,
-                                items: []
-                            }
-                        ]
-                    }
-                ]
-            },
-            {
-                name: 'Фильтры',
-                hasChildren: true,
-                items: 
-                [
-                    {
-                        name: 'Ulgran',
-                        hasChildren: true,
-                        items: 
-                        [
-                            {
-                                name: 'Smth',
-                                hasChildren: false,
-                                items: []
-                            },
-                            {
-                                name: 'Smth',
-                                hasChildren: false,
-                                items: []
-                            }
-                        ]
-                    },
-                    {
-                        name: 'Vigro Mramor',
-                        hasChildren: false,
-                        items: []
-                    }
-                ]
+class Pizza {
+    constructor(stuff, price, calories, size){
+        this.stuff = stuff;
+        this.price = price;
+        this.calories = calories;
+        this.size = size;
+        this.toppings = []
+    }
+
+    addTopping(topping){
+        if(topping instanceof Topping){
+            this.toppings.push(topping);
+            return;
+        }
+        console.log("Это не добавка!")
+    }
+
+    removeTopping(topping){
+        //плохая идея, остается пустой элемент
+        // const index = this.toppings.findIndex(e => e == topping);
+        // delete this.toppings[index];
+
+        //плохая идея №2, удаляются все добавки одного вида(мб можно использовать 2х добавки, не написано)
+        //this.toppings = this.toppings.filter(element => element !== topping)
+        
+        //система боль, но работает
+        // const index = this.toppings.findIndex(e => e == topping);
+        // if(index == -1){
+        //     return;
+        // }
+
+        // let array = new Array();
+
+        // for(let i = 0; i < this.toppings.length; i++){
+        //     if(i != index){
+        //         array.push(this.toppings[i]);
+        //     }   
+        // }
+
+        // this.toppings = array;
+        const index = this.toppings.findIndex(e => e == topping);
+        this.toppings.splice(index, 1);
+    }
+
+    getToppings(){
+        return this.toppings.map(element =>
+            element.name
+        );
+    }
+
+    getSize(){
+        return this.size;
+    }
+
+    getStuffing(){
+        return this.stuff;
+    }
+
+    calculatePrice(){
+        let result = this.price;
+        this.toppings.forEach(element =>{
+            if (this.size == "Большая"){
+                result += element.bigPrice;
             }
-        ]
+            else{
+                result += element.smallPrice;
+            }
+        }) 
+        return result;   
     }
 
+    calculateCalories(){
+        let result = this.calories;
+        this.toppings.forEach(element =>{
+            if (this.size == "Большая"){
+                result += element.bigCalories;
+            }
+            else{
+                result += element.smallCalories;
+            }
+        }) 
+        return result;   
+    }
+}
 
-    const items = new ListItems(document.getElementById('list-items'), data)
-
-    items.render()
-    items.init()
-
-    function ListItems(el, data) 
-    {
-        this.el = el;
-        this.data = data;
-
-        this.init = function () 
-        {
-            const parents = this.el.querySelectorAll('[data-parent]')
-
-            parents.forEach(parent => 
-                {
-                const open = parent.querySelector('[data-open]')
-
-                open.addEventListener('click', () => this.toggleItems(parent) )
-            })
+class Margherita extends Pizza{
+    constructor(size){
+        if (size == "Большая"){
+            super("Маргарита", 700, 500, size)
         }
-
-        this.render = function () 
-        {
-            this.el.insertAdjacentHTML('beforeend', this.renderParent(this.data))
+        else if (size == "Маленькая"){
+            super("Маргарита", 600, 400, size)
         }
-
-        this.renderParent = function (data) 
-        {
-            let span = '<span>' + data.name + '</span>';
-            let elParent = '<div class="list-item list-item_open" data-parent><div class="list-item__inner">'
-            elParent += '<img class="list-item__arrow" src="img/chevron-down.png" alt="chevron-down" data-open>' +
-                '<img class="list-item__folder" src="img/folder.png" alt="folder">' + span + '</div>' +
-            '<div class="list-item__items">'
-            
-            data.items.forEach(item => 
-                {
-                if (item.hasChildren) 
-                    elParent+= this.renderParent(item);
-                else
-                    elParent+= this.renderChildren(item.name);
-            });
-
-            elParent += '</div> </div>';
-            return elParent
-        }
-
-        this.renderChildren = function (name) 
-        {
-            let span = '<span>' + name + '</span>';
-            let elChild ='<div class="list-item__inner">' +
-            '<img class="list-item__folder" src="img/folder.png" alt="folder">'+ span + '</div>';
-            return elChild;
-        }
-
-        this.toggleItems = function (parent) 
-        {
-            parent.classList.toggle('list-item_open')
+        else{
+            throw new Error("Размер указан неправильно!");
         }
     }
 }
+
+class Pepperoni extends Pizza{
+    constructor(size){
+        if (size == "Большая"){
+            super("Пепперони", 1000, 600, size)
+        }
+        else if (size == "Маленькая"){
+            super("Пепперони", 900, 500, size)
+        }
+        else{
+            throw new Error("Размер указан неправильно!");
+        }
+    }
+}
+
+class Bavarian extends Pizza{
+    constructor(size){
+        if (size == "Большая"){
+            super("Баварская", 900, 650, size)
+        }
+        else if (size == "Маленькая"){
+            super("Баварская", 800, 550, size)
+        }
+        else{
+            throw new Error("Размер указан неправильно!");
+        }
+    }
+}
+
+const Size = {
+    Big:"Большая",
+    Small:"Маленькая"
+}
+let margPizza = new Margherita(Size.Big);
+let bavarianPizza = new Bavarian(Size.Small);
+
+let array = [margPizza, bavarianPizza]
+let toppingCheeseBoard = new Topping("Сырный борт", 50, 20, 100, 40);
+let toppingCreamyMozzarella = new Topping("Сливочная моцарелла", 50, 20, 100, 50);
+let toppingCheddarParmesan = new Topping("Чедер и пармезан", 150, 50, 300, 100);
+const pizzaTest = new Pizza("Не число", "Не число" , "Не число", "Не число","Не число")
+margPizza.addTopping(toppingCheddarParmesan);
+margPizza.addTopping(toppingCheeseBoard);
+margPizza.addTopping("Не добавка");
+margPizza.removeTopping(toppingCheddarParmesan);
+const container = document.getElementById("container");
+
+
+
+let rez = "";
+for (const element of array){
+    rez = `${rez}<div>Пицца: ${element.getStuffing()} Цена: ${element.calculatePrice()} Добавки: ${element.getToppings()}</div>` 
+}
+container.innerHTML = `<div>${rez}</div>`
